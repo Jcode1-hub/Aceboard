79790d1aadd28e30daeed77d979b45d55d301672
Judah Kayode <sbqpmv9spf@privaterelay.appleid.com>
2026-08-06 02:39:28 +0000

    Add error boundary to surface runtime crash

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,32 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import AceBoard from "./App.jsx";
 
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { error: null };
+  }
+  static getDerivedStateFromError(error) {
+    return { error };
+  }
+  render() {
+    if (this.state.error) {
+      return (
+        <pre style={{ color: "red", padding: 20, fontSize: 12, whiteSpace: "pre-wrap" }}>
+          {this.state.error.toString()}
+          {"\n\n"}
+          {this.state.error.stack}
+        </pre>
+      );
+    }
+    return this.props.children;
+  }
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AceBoard />
+    <ErrorBoundary>
+      <AceBoard />
+    </ErrorBoundary>
   </React.StrictMode>
 );
